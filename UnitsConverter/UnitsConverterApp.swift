6b06b6a3da340c87725a23f4eefd446f788417a8
sebastian.popa <sebastian.popa@softvision.com>
2023-07-16 22:07:27 +0300

    Initial Commit

---
+++ UnitsConverter/UnitsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConverterApp.swift
+//  UnitsConverter
+//
+//  Created by sebastian.popa on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
